68d0d155c9ee74e23c3a9c3dcf27441580bc16cd
wildpow <element1028@gmail.com>
2019-01-25 22:48:28 -0800

    messing with react-spring, Rebass and styled-components
    have not made the menu yet :-)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import { Card, Image, Box, Text, Heading, Flex } from "rebass";
+import rover from "./marsRover.jpg";
+import "styled-components/macro";
+import { Spring, Transition } from "react-spring";
 class App extends Component {
   render() {
+    const mars = "Mars Rover!!!!".split("");
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+        {props => (
+          <Flex
+            width={[250, 450, 650, 850]}
+            css="transition: all 150ms ease-in"
+            style={props}
           >
-            Learn React
-          </a>
-        </header>
-      </div>
+            console.log(mars)
+            <Card
+              p={1}
+              borderRadius={2}
+              boxShadow="0 0 16px rgba(0, 0, 0, .25)"
+            >
+              <Image src={rover} />
+              <Box px={2}>
+                <Heading as="h3" fontSize={["2em", "3.5em", "5em", "6em"]}>
+                  <Transition
+                    items={mars}
+                    from={{ transform: "translate3d(0,-40px,0)" }}
+                    enter={{ transform: "translate3d(0,0px,0)" }}
+                    leave={{ transform: "translate3d(0,-40px,0)" }}
+                  >
+                    {item => props => (
+                      <Text style={props}>
+                        {console.log(item, props)} {item}
+                      </Text>
+                    )}
+                  </Transition>
+                </Heading>
+                <Text fontSize={[0, 1, 2]}>Cool picture of a Mars Rover!</Text>
+              </Box>
+            </Card>
+          </Flex>
+        )}
+      </Spring>
     );
   }
 }
